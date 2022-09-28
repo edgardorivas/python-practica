5271d507704e0f13a1f8b585a9a39f108cb64ccf
edgardo rivas <edgadorivas123@gmail.com>
2022-09-28 18:59:58 -0400

    cambios

---
+++ inicio/metodos.py
@@ -0,0 +1,14 @@
+from conexion import *
+
+class metodos:
+    def mostrarTodo(tabla):
+        try:
+            conn = ConexionBaseDatos()
+            
+            
+        
+        finally:
+            conn.close()
+
+        except exception:
+            return exception

2095fc06af0312b9bc9ecb5ed28ffd3ca133ee8c
edgardo rivas <edgadorivas123@gmail.com>
2022-10-04 18:01:37 -0400

    base de datos

--- projecto/myapp/models.py
+++ projecto/myapp/models.py
@@ -4,7 +4,10 @@ class TipoUsuario(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=100)
 
+
 class Usuario(models.Model):
     nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.TextField()
     cedula = models.CharField(max_length=50)
     tipoUsuario = models.ForeignKey(TipoUsuario, on_delete = models.CASCADE)

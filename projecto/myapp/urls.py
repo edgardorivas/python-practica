cd17807d188508de2c530cd7ca44d6dffecec9a3
edgardo rivas <edgadorivas123@gmail.com>
2022-10-07 14:55:35 -0400

    algo

--- projecto/myapp/urls.py
+++ projecto/myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('saludar/<str:nombre>',views.hello)    
+    path('saludar/<str:nombre>',views.hello), 
+    path('tipos/',views.registros),
+    path('tipos/<int:id>',views.unRegistro), 
+    path('tipos/eliminar/<int:id>',views.eliminar), 
+
 ]
--- projecto/myapp/views.py
+++ projecto/myapp/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse ,JsonResponse, Http404
+from .models import TipoUsuario
+from django.shortcuts import get_object_or_404
 
 def hello(request , nombre):
     return HttpResponse('hola %s' % nombre)
-# Create your views here.
+
+def registros(request):
+    tipos = list(TipoUsuario.objects.values())
+    return JsonResponse(tipos,safe=False)
+
+def unRegistro(request, id):
+    try:
+        #/valor = get_object_or_404(TipoUsuario, id=id)
+        valor = TipoUsuario.objects.get(id=id)
+        return HttpResponse('el nombre del tipo es : %s' % valor.nombre)
+    except TipoUsuario.DoesNotExist:
+        raise Http404('no se encontro ')
+
+def eliminar(request, id):
+    try:
+        valor = TipoUsuario.objects.get(id=id)
+        valor.delete()
+        return HttpResponse('Se elimino el elemento')
+    except TipoUsuario.DoesNotExist:
+        raise Http404('no se encontro el elemento a eliminar')
